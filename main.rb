3b71d52f265dbe28b298a38588d5ca584a3eb7db
imubarak234 <imubarak2424@gmail.com>
2022-03-07 14:18:46 +0100

    refactor-person.rb

--- main.rb
+++ main.rb
@@ -4,74 +4,9 @@ require_relative './book'
 require_relative './rental'
 require_relative './classroom'
 
-@books = []
-@people = []
+
 @rentals = []
 
-class list_books
-  def list_all_books
-    @books.each { |n| puts n.to_string }
-  end
-end
-
-class list_people 
-  def list_all_people
-    @people.each { |n| puts n.to_string }
-  end
-end
-
-class create_person
-  def create_a_student
-    default_classroom = Classroom.new('default')
-    puts 'Age: '
-    age = gets.chomp
-    puts 'Name: '
-    name = gets.chomp
-    puts 'Has parent permission? [Y/N]: '
-    permission = gets.chomp
-    student = if permission.upcase == 'Y'
-                Student.new(age, default_classroom, name, parent_permission: true)
-              else
-                Student.new(age, default_classroom, name, parent_permission: false)
-              end
-    @people.push(student)
-  end
-  
-  def create_a_teacher
-    puts 'Age: '
-    age = gets.chomp
-    puts 'Name: '
-    name = gets.chomp
-    puts 'Specilaization'
-    specialization = gets.chomp
-    teacher = Teacher.new(age, specialization, name)
-    @people.push(teacher)
-  end
-  
-  def create_a_person
-    puts 'Do you want to create a Student (1) or a Teacher (2)? [Input the number]: '
-    var = gets.chomp
-    case var
-    when '1'
-      create_a_student
-    when '2'
-      create_a_teacher
-    end
-    puts 'Person created Succesfully'
-  end
-end
-
-class create_book 
-  def create_a_book
-    print 'Title: '
-    title = gets.chomp
-    print 'Author: '
-    author = gets.chomp
-    book = Book.new(title, author)
-    @books.push(book)
-    puts 'Book created Successfully'
-  end
-end
 
 class creat_rental 
   def create_a_rental
--- person.rb
+++ person.rb
@@ -8,6 +8,7 @@ class Person
     @permission = parent_permission
     @correct = Corrector.new
     @rental = []
+    @people = []
   end
 
   attr_reader :id, :name, :age, :permission, :rental
@@ -34,6 +35,49 @@ class Person
 
   def add_rental(book, data)
     @rental.push(Rental.new(data, self, book))
+  end
+
+  def list_all_people
+    @people.each { |n| puts n.to_string }
+  end
+
+  def create_a_student
+    default_classroom = Classroom.new('default')
+    puts 'Age: '
+    age = gets.chomp
+    puts 'Name: '
+    name = gets.chomp
+    puts 'Has parent permission? [Y/N]: '
+    permission = gets.chomp
+    student = if permission.upcase == 'Y'
+                Student.new(age, default_classroom, name, parent_permission: true)
+              else
+                Student.new(age, default_classroom, name, parent_permission: false)
+              end
+    @people.push(student)
+  end
+  
+  def create_a_teacher
+    puts 'Age: '
+    age = gets.chomp
+    puts 'Name: '
+    name = gets.chomp
+    puts 'Specilaization'
+    specialization = gets.chomp
+    teacher = Teacher.new(age, specialization, name)
+    @people.push(teacher)
+  end
+  
+  def create_a_person
+    puts 'Do you want to create a Student (1) or a Teacher (2)? [Input the number]: '
+    var = gets.chomp
+    case var
+    when '1'
+      create_a_student
+    when '2'
+      create_a_teacher
+    end
+    puts 'Person created Succesfully'
   end
 
   private
