207fb20c05d141ad26e3e2265f8f61d31b9043f1
imubarak234 <imubarak2424@gmail.com>
2022-03-10 15:28:35 +0100

    classroom class

---
+++ spec/classroom_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe "Testing the Classrom class" do 
+  before :each do 
+    @classroom = Classroom.new("Label")
+  end
+
+  it "with the above parameter a Classroom object should be created" do
+    @classroom.should be_an_instance_of Classroom
+  end
+
+  it "returns the correct label" do
+    @classroom.label.should eql "Label"
+  end
+
+end
