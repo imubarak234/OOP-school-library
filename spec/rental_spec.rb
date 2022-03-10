994cccd05f7a95e0a7f6a45c75c8d57527c887c1
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-03-10 17:55:21 +0100

    Fix linter errors

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
-describe "Testing the Rental class" do
+describe 'Testing the Rental class' do
   before :each do
-    @person = Person.new(32,'name', parent_permission: true)
-    @book = Book.new("Title", "Author")
-    @rental = Rental.new("02-12-2022", @person, @book)
+    @person = Person.new(32, 'name', parent_permission: true)
+    @book = Book.new('Title', 'Author')
+    @rental = Rental.new('02-12-2022', @person, @book)
   end
-  it "with the above parameter a rental object should be created" do
+  it 'with the above parameter a rental object should be created' do
     @rental.should be_an_instance_of Rental
   end
-  it "returns the correct date" do
-    @rental.date.should eql "02-12-2022"
+  it 'returns the correct date' do
+    @rental.date.should eql '02-12-2022'
   end
 end
